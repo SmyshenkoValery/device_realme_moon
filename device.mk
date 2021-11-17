e997deef24c2c8db9a5715ea08566a4fb939dfdb
DPSLEGEND <82019599+DPSLEGEND@users.noreply.github.com>
2021-11-17 19:53:22 +0530

    moon:adding realme dirac

--- device.mk
+++ device.mk
@@ -25,6 +25,9 @@ $(call inherit-product, vendor/realme/moon/moon-vendor.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Realme Dirac
+$(call inherit-product-if-exists, packages/apps/RealmeDirac/dirac.mk)
+
 # Parts
 $(call inherit-product-if-exists, packages/apps/RealmeParts/parts.mk)
 
