e19ddf54360652fdbac399912ccbcf4e369b5615
DPSLEGEND <iqbal.singh.02.pb@gmail.com>
2021-12-27 15:45:17 +0530

    moon :octavi os bringup

--- octavi_moon.mk
+++ octavi_moon.mk
@@ -18,17 +18,20 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit common ArrowOS configurations
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit common Octavi configurations
+$(call inherit-product, vendor/octavi/config/common.mk)
+
+#Gapps
+WITH_GAPPS=true
 
 # Inherit from device makefile
 $(call inherit-product, device/realme/moon/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_moon
+PRODUCT_NAME := octavi_moon
 PRODUCT_DEVICE := moon
 PRODUCT_BRAND := realme
-PRODUCT_MODEL := Realme G95 Series
+PRODUCT_MODEL := Realme G85 Series
 PRODUCT_MANUFACTURER := realme
 
 TARGET_BOOT_ANIMATION_RES := 720
