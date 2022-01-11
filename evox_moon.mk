360e70af348fd7716aa29670e5598d8e30d4ab47
SmyshenkoValery <94898917+SmyshenkoValery@users.noreply.github.com>
2022-01-11 09:14:06 +0530

    moon: Evolution-X bringup

--- evox_moon.mk
+++ evox_moon.mk
@@ -18,17 +18,17 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit common ArrowOS configurations
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit common Evolution-X configurations
+$(call inherit-product, vendor/evox/config/common.mk)
 
 # Inherit from device makefile
 $(call inherit-product, device/realme/moon/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_moon
+PRODUCT_NAME := evox_moon
 PRODUCT_DEVICE := moon
 PRODUCT_BRAND := realme
-PRODUCT_MODEL := Realme G95 Series
+PRODUCT_MODEL := Realme G85 Series
 PRODUCT_MANUFACTURER := realme
 
 TARGET_BOOT_ANIMATION_RES := 720
